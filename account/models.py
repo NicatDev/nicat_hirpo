83a454dee32ba7315cc31a14d2175c5767ee4073
unknown <nicat254memmedov@gmail.com>
2023-05-03 19:05:24 +0400

    asaaaaqq

--- account/models.py
+++ account/models.py
@@ -8,9 +8,11 @@ class Activationcode(models.Model):
     user = models.ForeignKey(MyUser,on_delete=models.CASCADE,related_name='code')
     activation_code = models.CharField(max_length=40,null=True,blank=True)
     
+class ExcellFile(models.Model):
+    ecx = models.FileField(upload_to='/files')
 
-
-
+    def __str__(self):
+        return 'file'
 # class MyUserManager(BaseUserManager):
 #     def create_user(self,email,password=None,is_active=True,is_staff=False,is_superuser=False):
 #         if not email:
